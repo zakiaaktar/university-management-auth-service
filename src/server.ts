5159feb4167094689d5d6955245847468365a917
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-06-14 08:22:19 +0200

    [DONE] Create user service

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,18 @@
-import mongoose from 'mongoose';
-import config from './config/index';
-import app from './app';
-
-
-
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 //database connection
 async function bootstrap() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database Connection Successful`);
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database Connection Successful`)
 
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-        });
-    } catch (err) {
-        console.log(`Failed to connect database`, err);
-    }
-
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log(`Failed to connect database`, err)
+  }
 }
-bootstrap();
+bootstrap()
